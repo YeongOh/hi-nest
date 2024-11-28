fb0756e01307e08fec564608d13b17eed63b0a71
penny <yjo0723@hotmail.com>
2024-11-28 17:59:01 +0900

    REST done

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie title ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: number): Movie {
+    console.log(typeof id);
+    const movie = this.moviesService.getOne(id);
+    if (!movie) {
+      throw new NotFoundException('Movie with Id not found');
+    }
+    return movie;
+  }
+
+  @Post()
+  crate(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
